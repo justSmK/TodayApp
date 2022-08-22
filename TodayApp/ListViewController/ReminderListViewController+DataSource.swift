3776534b66a46ad82df33fcee1736b6d8d97083b
justSmK <semko431@gmail.com>
2022-08-22 18:23:40 +0300

    Changed the Cell Background Color

--- TodayApp/ListViewController/ReminderListViewController+DataSource.swift
+++ TodayApp/ListViewController/ReminderListViewController+DataSource.swift
@@ -19,6 +19,10 @@ extension ReminderListViewController {
         contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
         contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
         cell.contentConfiguration = contentConfiguration
+        
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = .todayListCellBackground
+        cell.backgroundConfiguration = backgroundConfiguration
     }
     
 }
