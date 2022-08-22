dcfba16ff4b7f096983378decb1554d1cb504f2b
justSmK <semko431@gmail.com>
2022-08-22 16:50:43 +0300

    Init project

---
+++ TodayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodayApp
+//
+//  Created by justSmK on 8/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
