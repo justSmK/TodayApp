eed48aeb258918430841e44c500c4114d8e029d4
justSmK <semko431@gmail.com>
2022-08-22 18:19:19 +0300

    Organized View Controllers

---
+++ TodayApp/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,24 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  TodayApp
+//
+//  Created by justSmK on 8/22/22.
+//
+
+import Foundation
+import UIKit
+
+extension ReminderListViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+    }
+    
+}
